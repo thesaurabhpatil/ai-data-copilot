6192537c7b9e6853fbd74146add555c287cb5999
Saurabh Patil <thesaurabhpatil26@gmail.com>
2026-03-23 16:44:37 +0530

    fix langchain text splitter import

--- app/pdf_utils.py
+++ app/pdf_utils.py
@@ -1,5 +1,5 @@
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 
 
