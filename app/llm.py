0387328ea31b40db29305973e55537f77e6cfda8
Saurabh Patil <thesaurabhpatil26@gmail.com>
2026-03-24 19:25:39 +0530

    fix llm model to working one

--- app/llm.py
+++ app/llm.py
@@ -2,7 +2,8 @@ from langchain_huggingface import HuggingFaceEndpoint
 
 def load_llm():
     return HuggingFaceEndpoint(
-        repo_id="google/flan-t5-base",
+        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+        task="text-generation",
         temperature=0.5,
-        max_new_tokens=512
+        max_new_tokens=512,
     )
