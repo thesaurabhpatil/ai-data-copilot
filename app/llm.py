02019695e86921295c4d31364cec15be89372c03
Saurabh Patil <thesaurabhpatil26@gmail.com>
2026-03-24 19:31:41 +0530

    fix model task to conversational

--- app/llm.py
+++ app/llm.py
@@ -3,7 +3,7 @@ from langchain_huggingface import HuggingFaceEndpoint
 def load_llm():
     return HuggingFaceEndpoint(
         repo_id="mistralai/Mistral-7B-Instruct-v0.2",
-        task="text-generation",
+        task="conversational",   
         temperature=0.5,
         max_new_tokens=512,
     )
