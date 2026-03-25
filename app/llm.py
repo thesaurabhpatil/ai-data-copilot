7d97daf6da6b930c1e9d2200b71484ef85a43407
Saurabh Patil <thesaurabhpatil26@gmail.com>
2026-03-25 15:34:34 +0530

    fix pipeline task to text-generation

--- app/llm.py
+++ app/llm.py
@@ -3,9 +3,9 @@ from transformers import pipeline
 
 def load_llm():
     pipe = pipeline(
-        "text2text-generation",
-        model="google/flan-t5-small",   # ✅ lightweight & works
-        max_new_tokens=512,
+        "text-generation",                     
+        model="distilgpt2",                    
+        max_new_tokens=200,
     )
 
     return HuggingFacePipeline(pipeline=pipe)
